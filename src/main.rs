86750418e8f83fd746aef71cfe755e656cdea65b
Sergei Minaev <brainsburn@yandex.ru>
2024-02-17 20:49:37 +0300

    v0.1.1

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 	}
 	let fname = &args[1];
 
-    let cm = Arc::<SourceMap>::default();
+	let cm = Arc::<SourceMap>::default();
 
 	let opts = config::Options {
 		config: config::Config {
@@ -31,13 +31,13 @@ fn main() {
 		..Default::default()
 	};
 
-    let c = swc::Compiler::new(cm.clone());
-    let output = GLOBALS.set(&Default::default(), || {
-            try_with_handler(cm.clone(), Default::default(), |handler| {
-                let fm = cm.load_file(Path::new(fname)).expect("failed to load file");
-                Ok(c.process_js_file(fm, handler, &opts).expect("failed to process file"))
-            })
-        }).unwrap();
+	let c = swc::Compiler::new(cm.clone());
+	let output = GLOBALS.set(&Default::default(), || {
+			try_with_handler(cm.clone(), Default::default(), |handler| {
+				let fm = cm.load_file(Path::new(fname)).expect("failed to load file");
+				Ok(c.process_js_file(fm, handler, &opts).expect("failed to process file"))
+			})
+		}).unwrap();
 
-    println!("{}", output.code);
+	println!("{}", output.code);
 }
