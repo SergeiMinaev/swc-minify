9e500896da5ea81657657acf23b16da1b0fbda5c
Sergei Minaev <brainsburn@yandex.ru>
2024-02-17 19:14:18 +0300

    v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# swc-minify-wrapper
+# swc-minify
 Simple binary wrapper around SWC to minify JS files. No custom config supported - just specify input file and it's done.
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::{ path::Path, sync::Arc, env };
+use swc::{ self, BoolOrDataConfig, BoolConfig, config, try_with_handler };
+use swc_common::{ GLOBALS, SourceMap };
+use swc_ecma_ast::EsVersion;
+
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		println!("Specify input file.");
+		return
+	}
+	let fname = &args[1];
+
+    let cm = Arc::<SourceMap>::default();
+
+	let opts = config::Options {
+		config: config::Config {
+			minify: BoolConfig::new(Some(true)),
+			jsc: config::JscConfig {
+				target: Some(EsVersion::EsNext),
+				minify: Some(config::JsMinifyOptions {
+					compress: BoolOrDataConfig::from_bool(true),
+					mangle: BoolOrDataConfig::from_bool(true),
+					..Default::default()
+				}),
+				..Default::default()
+			},
+			..Default::default()
+		},
+		..Default::default()
+	};
+
+    let c = swc::Compiler::new(cm.clone());
+    let output = GLOBALS.set(&Default::default(), || {
+            try_with_handler(cm.clone(), Default::default(), |handler| {
+                let fm = cm.load_file(Path::new(fname)).expect("failed to load file");
+                Ok(c.process_js_file(fm, handler, &opts).expect("failed to process file"))
+            })
+        }).unwrap();
+
+    println!("{}", output.code);
+}
